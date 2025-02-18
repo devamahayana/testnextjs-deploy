d90c4cf1c1ee99fb343f2ceae9b07f1d917db0bc
Deva Mahayana <devamahayanatop@gmail.com>
2025-02-18 15:08:18 +0800

    Add font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,9 @@
 import Menu from "@/components/Menu";
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-// import "./script";
+import localFont from 'next/font/local'
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+const myFont = localFont({ src: './fonts/TT Hoves Pro Trial Medium.ttf' })
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -25,7 +16,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className={myFont.className}>
       
       <body
         className="overflow-hidden"
