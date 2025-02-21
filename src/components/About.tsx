9fd0811ba6515425a17cb9956a4deb591c43381c
Deva Mahayana <devamahayanatop@gmail.com>
2025-02-21 17:08:14 +0800

    dasdsa

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -3,11 +3,13 @@ import React from 'react'
 const About = () => {
   return (
     <section className='h-screen w-full'>
-        <div className='relative h-screen w-full'>
-            <h3 className='text-4xl absolute m-auto left-0 right-0 top-0 bottom-0 text-center'>We live our best lives excelling at design and branding,
-            advertising, web development and everything we touch</h3>
-            <h1 className='text-[23rem] font-semibold tracking-wider absolute bottom-0 left-0 right-0 text-center'>BALIMMO</h1>
-        </div>
+      <h1 className='text-[16rem] font-semibold tracking-wide'>Intro</h1>
+      <h3 className='text-8xl font-semibold'>
+        Creative developer with a design background, crafting immersive digital experiences that combine creativity and functionality.
+      </h3>
+      {/* <h3 className='text-4xl text-center'>
+        We live our best lives excelling at design and branding,
+        advertising, web development and everything we touch</h3> */}
     </section>
   )
 }
