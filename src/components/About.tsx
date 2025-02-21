55a8c4b94c43c8b8b3985c9c32db10c956a37bd1
Deva Mahayana <devamahayanatop@gmail.com>
2025-02-21 17:14:50 +0800

    dasdas

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -4,9 +4,15 @@ const About = () => {
   return (
     <section className='h-screen w-full'>
       <h1 className='text-[16rem] font-semibold tracking-wide'>Intro</h1>
-      <h3 className='text-8xl font-semibold'>
-        Creative developer with a design background, crafting immersive digital experiences that combine creativity and functionality.
-      </h3>
+      <div className='flex items-end'>
+        <p className='text-2xl font-semibold w-1/4'>
+          Creative developer with a design background, crafting immersive digital experiences that combine creativity and functionality.
+        </p>
+        <h3 className='text-8xl font-semibold w-3/4'>
+          Creative developer with a design background, crafting immersive digital experiences that combine creativity and functionality.
+        </h3>
+        
+      </div>
       {/* <h3 className='text-4xl text-center'>
         We live our best lives excelling at design and branding,
         advertising, web development and everything we touch</h3> */}
